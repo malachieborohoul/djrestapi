6287fa1fc2c2fab5106eeb027a1d5c6d2c2917eb
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-20 22:13:52 +0100

    body json

--- api/views.py
+++ api/views.py
@@ -1,5 +1,11 @@
+import json 
 from django.http import JsonResponse
 
 def api_home(request, *arg, **kwargs):
     body=request.body
+    data={}
+    try:
+        data=json.loads(body)
+    except:
+        pass
     return JsonResponse({"message":"Hi there"})
